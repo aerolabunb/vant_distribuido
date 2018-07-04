968f31f07e0b6b176fa5bcdc9e2da78d7f7d50cc
Bruno Floriano <brunofloriano@github.com>
2018-07-04 10:25:25 -0300

    Timer

--- base_gdatalogger/gdatalogger/base/base_gdatalogger.c
+++ base_gdatalogger/gdatalogger/base/base_gdatalogger.c
@@ -14,12 +14,78 @@
 #include <termios.h>
 #include <fcntl.h>
 
+#include "time.h"
 #include "../gqueue.h"
 #include "../gmatlabdatafile.h"
 #include "../gdatalogger.h"
 #include <stdio.h>
 
+#define TASK_PERIOD_US                  200000
+#define PI                              3.14159265
+
+int timer_nr;
+timer_t timer;
+void timer_start (void);
+void timer_stop (void);
+void controle (union sigval sigval);
+
 GDATALOGGER gDataLogger;
+
+struct termios tty;
+struct termios tty_old;
+    
+static timestruct_t timestruct;
+
+
+void timer_start (void)
+{
+    struct itimerspec itimer = { { 1, 0 }, { 1, 0 } };
+    struct sigevent sigev;
+
+    itimer.it_interval.tv_sec=0;
+    itimer.it_interval.tv_nsec=TASK_PERIOD_US * 1000;
+    itimer.it_value=itimer.it_interval;
+
+    memset (&sigev, 0, sizeof (struct sigevent));
+    sigev.sigev_value.sival_int = timer_nr;
+    sigev.sigev_notify = SIGEV_THREAD;
+    sigev.sigev_notify_attributes = NULL;
+    sigev.sigev_notify_function = controle;
+
+//    if (timer_create (CLOCK_MONOTONIC, &sigev, &timer) < 0)
+    if (timer_create (CLOCK_REALTIME, &sigev, &timer) < 0)
+    {
+        fprintf (stderr, "[%d]: %s\n", __LINE__, strerror (errno));
+        exit (errno);
+    }
+
+    if (timer_settime (timer, 0, &itimer, NULL) < 0)
+    {
+        fprintf (stderr, "[%d]: %s\n", __LINE__, strerror (errno));
+        exit (errno);
+    }
+}
+
+void timer_stop (void)
+{
+    if (timer_delete (timer) < 0)
+    {
+        fprintf (stderr, "[%d]: %s\n", __LINE__, strerror (errno));
+        exit (errno);
+    }
+}
+
+void controle(union sigval arg){
+    
+    	int T;
+	T = time_gettime(&timestruct);
+	time_reset(&timestruct);
+    
+    	T = time_gettime(&timestruct);
+   	time_reset(&timestruct);
+
+}
+
 
 int main(){
 	double t = 1;
@@ -35,7 +101,9 @@ int main(){
 	gDataLogger_IPCUpdate(&gDataLogger); // gerencia IPC
         //gDataLogger_MatfileUpdate(&gDataLogger); // esvazia os buffers no arquivo de log
 	gDataLogger_Close(&gDataLogger);
-
+	timer_start ();
+	time_reset(&timestruct);
+	timer_stop ();
 
 return 0;
 }
